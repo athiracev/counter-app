c9b487381bdb507c422f30b8a3f8e7460325ac45
athiracev <athiracev@gmail.com>
2024-05-02 22:23:37 +0530

    counter-app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CounterMain from './components/CounterMain'
+
+function App() {
+
+  return (
+    <>
+     <CounterMain/>
+      
+    </>
+  )
+}
+
+export default App
